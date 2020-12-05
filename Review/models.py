20a36f9d64e1ed12f49c784a1f9702ab19303e30
SPARSH <sparshbatta2000@gmail.com>
2020-12-05 09:23:22 +0530

    Subsequent commit

--- Review/models.py
+++ Review/models.py
@@ -13,3 +13,6 @@ class ReviewOrder(models.Model):
     client=models.ForeignKey(Client,on_delete=models.CASCADE)
     review=models.TextField(default='',blank=True,max_length=1000)
     rating=models.CharField(choices=choices,default=Good,max_length=15)
+
+    def __str__(self):
+        return "{} - {}".format(self.client.user.username,self.rating)
